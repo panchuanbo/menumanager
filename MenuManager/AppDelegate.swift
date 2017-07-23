129e2eac95036be695fc545c3435ee201b92acf1
Chuanbo Pan <panchuanbo@rescomp-16-347342.stanford.edu>
2017-07-22 21:53:22 -0700

    Initial Commit

---
+++ MenuManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MenuManager
+//
+//  Created by Chuanbo Pan on 7/22/17.
+//  Copyright © 2017 Chuanbo Pan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MenuManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MenuManager
+//
+//  Created by Chuanbo Pan on 7/22/17.
+//  Copyright © 2017 Chuanbo Pan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
