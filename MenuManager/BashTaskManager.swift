b5448d597bb0dd53546db6b6e7f4c747cd21d0b6
Chuanbo Pan <panchuanbo@DN800c4f07.stanford.edu>
2017-09-04 11:48:04 -0700

    initial commit

---
+++ MenuManager/BashTaskManager.swift
@@ -0,0 +1,13 @@
+//
+//  BashTaskManager.swift
+//  MenuManager
+//
+//  Created by Chuanbo Pan on 7/23/17.
+//  Copyright © 2017 Chuanbo Pan. All rights reserved.
+//
+
+import Cocoa
+
+class BashTaskManager: NSObject {
+
+}
