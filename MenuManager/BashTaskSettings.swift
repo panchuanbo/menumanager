4989ac90f23cbb6881fb20a90b251c895119165c
Chuanbo Pan <panchuanbo@chuanbos-macbook-pro.local>
2017-09-11 23:15:26 -0700

    forgot a file

---
+++ MenuManager/BashTaskSettings.swift
@@ -0,0 +1,24 @@
+//
+//  BashTaskSettings.swift
+//  MenuManager
+//
+//  Created by Chuanbo Pan on 9/11/17.
+//  Copyright © 2017 Chuanbo Pan. All rights reserved.
+//
+
+import Cocoa
+
+struct BashTaskSettings {
+    var name: String!
+    var launchPath: String!
+    var params: [String]?
+    var useBashC: Bool!
+    var UUID: String!
+    
+    init(name: String, launchPath: String, params: [String]?, useBashC: Bool) {
+        self.name = name
+        self.launchPath = launchPath
+        self.params = params
+        self.useBashC = useBashC
+    }
+}
